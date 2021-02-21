f969affb98daf39aa3a3bb86e63c8b0067cd04fb
Yauheni <ev.drozd.2020@mail.ru>
2021-02-21 20:28:30 +0300

    add section food

--- app/js/script.js
+++ app/js/script.js
@@ -68,3 +68,29 @@ $(document).ready(function() {
 })
 
 
+
+let container = document.querySelector('.wr-food__container');
+
+function clearClass(arrElements, classNameEl) {
+    for (let i = 0; i < arrElements.length; i++) {
+      const box = arrElements[i];
+      if(box.classList.contains(classNameEl)) {
+        box.classList.remove(classNameEl)
+      }
+    }
+}
+
+container.addEventListener('mouseover', function(e) {
+    debugger    
+    let currentEl = e.target.closest('.wr-food__box');
+    if(!currentEl.classList.contains('wr-food__box')) return;
+
+    let block = currentEl;
+    if(block.classList.contains('food-active')) return;
+
+    let blocks = this.querySelectorAll('.wr-food__box');
+    clearClass(blocks, 'food-active');
+    block.classList.add('food-active');
+})
+
+
